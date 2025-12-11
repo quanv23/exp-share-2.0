53f5d7b4b9324938c6dbc10766b03140befaa347
Victor Quan <victorquan@gmail.com>
2025-12-10 19:37:37 -0500

    Implemented nav footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,47 +1,26 @@
-import { Link, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
 import Categories from './pages/Categories';
 import Category from './pages/Category';
 import Expenses from './pages/Expenses';
 import Notes from './pages/Notes';
-import Footer from './layouts/Footer';
+import Layout from './layouts/Layout';
 
 export default function App() {
 	return (
-		<>
-			<nav>
-				<ul>
-					<li>
-						<Link to="/">Home</Link>
-					</li>
-					<li>
-						<Link to="/dashboard">Dashboard</Link>
-					</li>
-					<li>
-						<Link to="/categories">Categories</Link>
-					</li>
-					<li>
-						<Link to="/expenses">Expenses</Link>
-					</li>
-					<li>
-						<Link to="/notes">Notes</Link>
-					</li>
-				</ul>
-			</nav>
-			<Routes>
-				<Route element={<Footer />}>
-					<Route path="/" element={<Home />} />
-					<Route path="/dashboard" element={<Dashboard />} />
-					<Route path="/categories">
-						<Route index element={<Categories />} />
-						<Route path=":id" element={<Category />} />
-					</Route>
-					<Route path="/expenses" element={<Expenses />} />
-					<Route path="/notes" element={<Notes />} />
-					<Route path="*" element={<h2>Page Not Found</h2>} />
+		<Routes>
+			<Route element={<Layout />}>
+				<Route path="/" element={<Home />} />
+				<Route path="/dashboard" element={<Dashboard />} />
+				<Route path="/categories">
+					<Route index element={<Categories />} />
+					<Route path=":id" element={<Category />} />
 				</Route>
-			</Routes>
-		</>
+				<Route path="/expenses" element={<Expenses />} />
+				<Route path="/notes" element={<Notes />} />
+				<Route path="*" element={<h2>Page Not Found</h2>} />
+			</Route>
+		</Routes>
 	);
 }
---
+++ frontend/src/layouts/Layout.tsx
@@ -0,0 +1,58 @@
+import { Outlet, NavLink } from 'react-router-dom';
+import {
+	HiChartBar,
+	HiCurrencyDollar,
+	HiFolder,
+	HiPlusCircle,
+	HiTag,
+} from 'react-icons/hi2';
+
+export default function Layout() {
+	return (
+		<div className="min-h-screen flex flex-col pb-15">
+			<Outlet />
+			<footer className="centered-flex gap-10 p-4 w-screen fixed bottom-0 left-0 shadow-lg bg-white navbar-shadow">
+				<NavLink
+					to="/"
+					className={({ isActive }) =>
+						`${isActive ? `text-black` : `text-myDarkGray`}`
+					}
+				>
+					<HiPlusCircle size={22} />
+				</NavLink>
+				<NavLink
+					to="/dashboard"
+					className={({ isActive }) =>
+						`${isActive ? `text-black` : `text-myDarkGray`}`
+					}
+				>
+					<HiChartBar size={22} />
+				</NavLink>
+				<NavLink
+					to="/categories"
+					className={({ isActive }) =>
+						`${isActive ? `text-black` : `text-myDarkGray`}`
+					}
+				>
+					<HiTag size={22} />
+				</NavLink>
+				<NavLink
+					to="/expenses"
+					className={({ isActive }) =>
+						`${isActive ? `text-black` : `text-myDarkGray`}`
+					}
+				>
+					<HiCurrencyDollar size={22} />
+				</NavLink>
+				<NavLink
+					to="/notes"
+					className={({ isActive }) =>
+						`${isActive ? `text-black` : `text-myDarkGray`}`
+					}
+				>
+					<HiFolder size={22} />
+				</NavLink>
+			</footer>
+		</div>
+	);
+}
